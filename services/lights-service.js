500f2cf69df3b28196be84dd233cada3da831b5c
Matthew Anguelo <m.anguelo@me.com>
2022-07-28 14:49:05 -0400

    Set lifx time

--- services/lights-service.js
+++ services/lights-service.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 import { Octokit } from "octokit";
-import { getColorForMessage, getHexColorForMessage, getSceneForMessage, shouldDisplayPendingAction } from "../utils/color-util.js";
+import {
+  getColorForMessage,
+  getHexColorForMessage,
+  getSceneForMessage,
+  shouldDisplayPendingAction,
+} from "../utils/color-util.js";
 
 const goveeClient = axios.create({
   baseURL: "https://developer-api.govee.com/v1",
@@ -12,7 +17,7 @@ const goveeClient = axios.create({
 const lifxClient = axios.create({
   baseURL: "https://api.lifx.com/v1",
   headers: {
-    'Authorization': `Bearer ${process.env.LIFX_API_KEY}`,
+    Authorization: `Bearer ${process.env.LIFX_API_KEY}`,
   },
 });
 
@@ -75,31 +80,31 @@ export class LightsService {
   /**
    * Sets the static light color for a message.
    * If a message is considered pending an action, the move effect with be applied.
-   * 
-   * @param {*} message 
-   * @param {*} ct 
+   *
+   * @param {*} message
+   * @param {*} ct
    */
   async setColorLifx(message, ct) {
     ct.throwIfCancelled();
 
     const sceneName = getSceneForMessage(message);
 
-    let scenes = await lifxClient.get("/scenes")
-    let scene = scenes.data.find((v) => v.name == sceneName)
+    let scenes = await lifxClient.get("/scenes");
+    let scene = scenes.data.find((v) => v.name == sceneName);
 
     await lifxClient.put(`/scenes/scene_id:${scene.uuid}/activate`);
 
     if (shouldDisplayPendingAction(message)) {
-      await this.moveEffectLifx(ct)
+      await this.moveEffectLifx(ct);
     }
   }
 
   /**
    * Applies the breathe effect for 5 cyles.
-   * 
-   * @param {*} message 
-   * @param {*} ct 
-   * @returns 
+   *
+   * @param {*} message
+   * @param {*} ct
+   * @returns
    */
   async breatheEffectLifx(message, ct) {
     ct.throwIfCancelled();
@@ -111,67 +116,73 @@ export class LightsService {
     }
 
     await lifxClient.post("/lights/all/effects/breathe", {
-      "period": 3,
-      "cycles": 5,
-      "color": color,
-      "from_color": "#111111",
-      "power_on": true,
-      "persist": false
+      period: 3,
+      cycles: 1,
+      color: color,
+      from_color: "#111111",
+      power_on: true,
+      persist: false,
     });
   }
 
   /**
    * Applies the move effect infinitely with a period of 10 seconds.
-   * 
-   * @param {*} message 
-   * @param {*} ct 
+   *
+   * @param {*} message
+   * @param {*} ct
    */
   async moveEffectLifx(ct) {
     ct.throwIfCancelled();
 
     await lifxClient.post("/lights/all/effects/move", {
-      "period": 10,
+      period: 10,
     });
   }
 
   /**
    * Disables all effects.
-   * 
-   * @param {*} ct 
+   *
+   * @param {*} ct
    */
   async disableEffects(ct) {
-    ct.throwIfCancelled()
+    ct.throwIfCancelled();
 
-    await lifxClient.post("/lights/all/effects/off")
+    await lifxClient.post("/lights/all/effects/off");
   }
 
   /**
    * Updates a log file on GitHub to display ammount of errors in a single day.
    * The log file will be reset if it is a new day.
-   * 
-   * @param {*} ct 
+   *
+   * @param {*} ct
    */
   async updateGitHubErrors(ct) {
-    ct.throwIfCancelled()
+    ct.throwIfCancelled();
 
     let logs = await octokit.rest.repos.getContent({
-      owner: 'manguelo',
+      owner: "manguelo",
       repo: process.env.LOGS_REPO,
-      path: 'logs.json',
-    })
+      path: "logs.json",
+    });
 
-    let content = JSON.parse(Buffer.from(logs.data.content, 'base64').toString('ascii'))
-    let currentTime = new Date(Date.now())
-    let lastModified = new Date(Date.parse(content.last_modified))
-    var reset = currentTime.getDate() > lastModified.getDate()
+    let content = JSON.parse(
+      Buffer.from(logs.data.content, "base64").toString("ascii")
+    );
+    let currentTime = new Date(Date.now());
+    let lastModified = new Date(Date.parse(content.last_modified));
+    var reset = currentTime.getDate() > lastModified.getDate();
 
     await octokit.rest.repos.createOrUpdateFileContents({
-      owner: 'Manguelo',
+      owner: "Manguelo",
       repo: process.env.LOGS_REPO,
-      path: 'logs.json',
-      message: 'Update logs',
+      path: "logs.json",
+      message: "Update logs",
       sha: logs.data.sha,
-      content: Buffer.from(`{"errors": ${reset ? 1 : content.errors + 1}, "last_modified": "${currentTime.toISOString()}"}`).toString('base64')
-    })
+      content: Buffer.from(
+        `{"errors": ${
+          reset ? 1 : content.errors + 1
+        }, "last_modified": "${currentTime.toISOString()}"}`
+      ).toString("base64"),
+    });
   }
 }
